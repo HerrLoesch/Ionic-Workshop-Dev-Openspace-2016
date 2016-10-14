31a76c0113cdc0e8ec7be204dba0a0865855cb67
Hendrik Lösch <loeschhendrik@googlemail.com>
2016-10-14 16:20:24 +0200

    formulare

--- src/pages/favorite-input-page/favorite-input-page.ts
+++ src/pages/favorite-input-page/favorite-input-page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavParams, ViewController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   templateUrl: 'favorite-input-page.html'
@@ -9,15 +10,29 @@ export class FavoriteInputPage {
   title: string;
   year: string;
 
+  formData: FormGroup;
+
   cancel(){
     this.viewController.dismiss();
   }
 
-  constructor(public navParams: NavParams, private viewController: ViewController) {
+  save() {
+    console.log(this.formData.value.commentsArea)
+    
+    this.viewController.dismiss(); 
+   }
+
+  constructor(public navParams: NavParams, 
+  private viewController: ViewController,
+  private formBuilder: FormBuilder) {
 
     this.title = navParams.get('Title');
     this.year = navParams.get('Year');
     
+    this.formData = this.formBuilder.group(
+        {commentsArea: ['', Validators.minLength(2)]}
+    );
+
   }
 
 
